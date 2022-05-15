6329baae26a5e8ceff7efc9c884f79484e23634b
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-05-15 00:01:46 -0500

    Adding Myapp

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -12,6 +12,25 @@ import {
 import { chain, createClient, WagmiProvider } from 'wagmi';
 
 function MyApp({ Component, pageProps }) {
+  const { chains, provider } = configureChains(
+    [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+    [
+      apiProvider.alchemy(process.env.ALCHEMY_ID),
+      apiProvider.fallback()
+    ]
+  );
+  
+  const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    chains
+  });
+  
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+  })
+  
   return <Component {...pageProps} />
 }
 
