5863cd3ffc46cc7bb634c621cdbc5a3b89e7e000
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-06-06 23:51:03 -0500

    Adding new const

--- components/ButMeACoffee.js
+++ components/ButMeACoffee.js
@@ -6,8 +6,8 @@ const ButMeACoffee = () => {
     const contactAddress = "0xaF4c182F7c1BbD5B014b930a5b2A8c23FF418C0e";
 
     return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        RAFA
+    <div>
+        
     </div>
   )
 }
