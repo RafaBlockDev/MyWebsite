71f772bffe155bb0146aa6b62cdee2734c7ea19f
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-06-28 00:10:42 -0500

    Using components from library

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Button } from "@nextui-org/react";
+import { Container, Card, Button, Text, Spacer } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button color="gradient">
-        Connect
-      </Button>
-    </div>
+    <Container xl>
+      <Card justify="center" align="center">
+        <Text h1 size={60}>Haloooo 👋🏻</Text>
+        <Spacer y={0.25}/>
+        <Text h1 size={60}>I´m Rafael</Text>
+        <Spacer y={0.25}/>
+        <Text p size={20}>Blockchain and web3 developer, consultant and researcher 🌎</Text>
+        <Spacer y={2}/>
+        <Button color="gradient">
+          Connect
+        </Button>
+      </Card>
+    </Container>
   )
 }
